587376b0b3db28eff4a444db75bb3f97dc22e2f7
phoenix377 <phoenix.number377@gmail.com>
2019-03-19 19:12:27 -0600

    change API endpoint

--- FutureRenewables/src/configs/environments.js
+++ FutureRenewables/src/configs/environments.js
@@ -4,14 +4,14 @@ export const ENV_PRODUCTION = 'production';
 export const APP_ENV = {
   staging: {
     env: 'staging',
-    apiUrl: 'https://api.staging.futurerenewablesfund.com.au/api/v1/',
+    apiUrl: 'https://hub.futurerenewablesfund.com.au/api/v1/',
     smsCode: '33',
     kleberKey: 'RK-A7331-D6238-189D0-FC8A7-C25ED-1B676-3133A-3122F',
     kleberGUID: 'a4ebd32f-2cef-4389-9ae2-288b3b7a9ee2',
   },
   production: {
     env: 'production',
-    apiUrl: 'https://api.staging.futurerenewablesfund.com.au/api/v1/',
+    apiUrl: 'https://hub.futurerenewablesfund.com.au/api/v1/',
     smsCode: '',
     kleberKey: 'RK-A7331-D6238-189D0-FC8A7-C25ED-1B676-3133A-3122F',
     kleberGUID: 'a4ebd32f-2cef-4389-9ae2-288b3b7a9ee2',
