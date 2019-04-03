39bd47c5935ce10f16f323c8e8c6b3750b48dc83
Andrew S <andrewmsellen@gmail.com>
2019-04-03 13:57:17 +1100

    Trying to rename file styleConstants by deleting then reuploading

---
+++ FutureRenewables/src/Styles/styleConstants.js
@@ -0,0 +1,46 @@
+
+export default Object.freeze({
+  mainColor: 'green',
+  containerBgColor: '#F3F3F3',
+  color: {
+    primary: '#FCCD3B',
+    secondary: '#CCCCCC',
+    light: '#FAFAFA',
+    dark: '#000000',
+    danger: 'red',
+    success: 'green',
+    gray: '#919191',
+    gray2: '#E8E9E8',
+    gray3: '#6A6A6A',
+    gray4: '#5C5C5C',
+    gray5: '#CFCFCF',
+    gray6: '#D8D8D8',
+    lightGreen: '#6FCE1C',
+    errorBorder: '#ed2f2f',
+    white: 'white',
+  },
+  fontFamily: 'Lato',
+  fontSize: {
+    h1: 30,
+    h2: 24,
+    h3: 18,
+    h4: 14,
+    p: 12,
+  },
+  fontWeight: {
+    h1: 900,
+    h2: 700,
+    h3: 300,
+    h4: 700,
+    p: 300,
+  },
+  radius: 8,
+  padding: {
+    container: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+  },
+  keyboardAvoidingHeight: 100,
+  inputHeightBase: 50,
+});
