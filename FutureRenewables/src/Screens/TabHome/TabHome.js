e2f0649dc4336a7572459ced36b5d0b137450f50
phoenix377 <phoenix.number377@gmail.com>
2019-04-10 18:16:36 -0600

    home tab

--- FutureRenewables/src/Screens/TabHome/TabHome.js
+++ FutureRenewables/src/Screens/TabHome/TabHome.js
@@ -9,14 +9,49 @@ import {
 import {
   Text,
   Content,
+  Button,
+  Icon,
+  H1,
+  Grid,
+  Col,
+  Row,
 } from 'native-base';
+
+import {
+  sg,
+} from 'src/Styles';
+import styles from './styles';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TabHome extends Component {
   render() {
     return (
-      <Content>
-        <Text>Home tab</Text>
+      <Content padder>
+        <View>
+          <Grid>
+            <Row>
+              <Icon name="add" />
+              <Text>asd</Text>
+            </Row>
+            <Row>
+              <Text note>asd</Text>
+            </Row>
+          </Grid>
+        </View>
+
+
+        <View>
+          <Button
+            transparent
+            iconRight
+            style={sg.mB10}
+          >
+            <Text style={styles.title}>Grace</Text>
+            <Icon name="ios-arrow-down" style={styles.titleIcon} />
+          </Button>
+
+          <H1 style={styles.mainAmount}>$1,978</H1>
+        </View>
       </Content>
     );
   }
---
+++ FutureRenewables/src/Screens/TabHome/styles.js
@@ -0,0 +1,14 @@
+
+export default {
+  mainAmount: {
+    fontSize: 50,
+    lineHeight: 50,
+  },
+  title: {
+    fontSize: 26,
+    paddingLeft: 0,
+  },
+  titleIcon: {
+    fontSize: 18,
+  },
+};
