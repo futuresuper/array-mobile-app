b6e2617e83270de3262ab9cc7153e90ed2ff41a8
Andrew S <andrewmsellen@gmail.com>
2019-04-18 16:55:56 +1000

    Added mock api data file for testing

---
+++ FutureRenewables/src/assets/testdata/testData.js
@@ -0,0 +1,177 @@
+const content = {
+  impact: [
+    {
+      "number": 9,
+      "text", "Solar farms owned"
+    },
+    {
+      "number": 12,
+      "suffix": "k",
+      "text", "Homes powered"
+    },
+    {
+      "number": 50,
+      "suffix": "t",
+      "text", "Of carbon removed"
+    },
+  ]
+  latest: [
+    {
+      "cardType": "small",
+      "headline": "Make an impact with your first deposit 🌟",
+      "actionToPage": "Deposit"
+    },
+    {
+      "cardType": "large",
+      "headline": "Take a look at exactly where your money goes",
+      "subhead": "Behind the scenes",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJcp56MsQXfjPlXs5ef88YvJg4WLXr1LzU4Fyvr7SDrEJWmCgJbg",
+      "article": [
+        "image": {
+          "url": "https://onestepoffthegrid.com.au/wp-content/uploads/2018/12/rsz_solar_farm_sunset.jpg"
+          "description": "Chinchilla Solar Farm"
+        },
+        "paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Arcu cursus euismod quis viverra. Ac auctor augue mauris augue. Nisi scelerisque eu ultrices vitae auctor eu augue. Venenatis a condimentum vitae sapien pellentesque habitant. Velit scelerisque in dictum non consectetur a erat.",
+        "paragraph": "Suspendisse ultrices gravida dictum fusce ut. Tortor condimentum lacinia quis vel eros donec ac odio. A pellentesque sit amet porttitor eget dolor morbi. Lectus urna duis convallis convallis tellus. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat.",
+        "heading": "This is a subheading in the middle of the article",
+        "paragraph": "Convallis aenean et tortor at risus viverra adipiscing at. Ornare suspendisse sed nisi lacus sed viverra. Mus mauris vitae ultricies leo integer malesuada nunc.",
+        "image": {
+          "url": "https://s3.envato.com/files/236059013/01_preview1.jpg"
+        },
+        "paragraph": "Felis eget nunc lobortis mattis aliquam faucibus. Et magnis dis parturient montes. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien.",
+        "image": {
+          "url": "http://static4.businessinsider.com/image/595e525dd084cc351c8b6e48-889/panda-green-energy-889x500.jpg",
+          "description": "Panda solar farm"
+        },
+        "image": {
+          "url": "https://www.solar-trade.org.uk/wp-content/uploads/2015/03/Sunset-at-Chalcroft-Solar-Farm.jpg",
+          "description": "Another solar farm"
+        },
+      ]
+    }
+  ]
+}
+
+
+const users = {
+  andrew: {
+    firstName: "Andrew",
+    lastName: "Sellen",
+    dateJoined: "2019-03-15",
+    profileImage: "https://media.licdn.com/dms/image/C5603AQFIBa4K4TCT4A/profile-displayphoto-shrink_200_200/0?e=1560988800&v=beta&t=HKxHAyjAlOKwQWKdFi42qonYBx4BaJzpbYRcypeMyC4"
+    email: "andrewmsellen@gmail.com",
+    residentialAddressUnitNumber: "4b",
+    residentialAddressStreetNumber: "30",
+    residenitalAddressStreet: "Woodward",
+    residentialAddressStreetType: "Ave",
+    residentialAddressSuburb: "Stanhope Gardens",
+    residentialAddressState: "NSW",
+    residentialAddressPostcode: "2768",
+    residentialAddressCountry: "Australia",
+    mobilePretty: "0402 239 471",
+    "accounts": [
+      {
+        "accountId": 1,
+        "accountType": "individual",
+        "accountNickName": "Andrew Sellen",
+        "monthlyInvestmentAmount": 20,
+        "initialInvestmentAmount": 1000,
+        "bankAccountName": "Andrew and Melissa Sellen",
+        "bankAccountBSB": "923 100",
+        "bankAccountNumber": "123456789",
+        "balanceDollars": 1264.73,
+      },
+      {
+        "accountId": 3,
+        "accountType": "adultForChild",
+        "accountNickName": "Chloe Sellen",
+        "initialInvestmentAmount": 30,
+        "bankAccountName": "Andrew and Melissa Sellen",
+        "bankAccountBSB": "923 100",
+        "bankAccountNumber": "123456789",
+        "balanceDollars": 32.45,
+      }
+    ],
+    "entities_linked_to": [
+      {
+        "entityId": 1,
+        "role": "owner"
+        "admin": true,
+        "type": "joint",
+        "name": "Andrew and Melissa Sellen",
+        "accounts": [
+          "accountId": 2,
+          "accountType": "joint",
+          "accountNickName": "Andrew and Melissa Sellen",
+          "monthlyInvestmentAmount": 10,
+          "initialInvestmentAmount": 5000,
+          "bankAccountName": "Andrew and Melissa Sellen",
+          "bankAccountBSB": "923 100",
+          "bankAccountNumber": "123456789",
+          "balanceDollars": 5067.12,
+        ]
+      },
+    ],
+  },
+  liv: {
+    firstName: "Olivia",
+    lastName: "King",
+    dateJoined: "2019-03-12",
+    profileImage: "https://media.licdn.com/dms/image/C5603AQFimW9SGIedDQ/profile-displayphoto-shrink_800_800/0?e=1560988800&v=beta&t=qaHmWU360GfkuwU2z-KJEPmBy-NeGbp5jBAg7-io3Is"
+    email: "liv@oliviaking.com",
+    residentialAddressStreetNumber: "149",
+    residenitalAddressStreet: "Pitt",
+    residentialAddressStreetType: "St",
+    residentialAddressSuburb: "Redfern",
+    residentialAddressState: "NSW",
+    residentialAddressPostcode: "2016",
+    residentialAddressCountry: "Australia",
+    mobilePretty: "0432 276 069",
+    "accounts": [
+      {
+        "accountId": 4,
+        "accountType": "individual",
+        "accountNickName": "Olivia King",
+        "initialInvestmentAmount": 200,
+        "bankAccountName": "Olivia King",
+        "bankAccountBSB": "923 100",
+        "bankAccountNumber": "123456789",
+        "balanceDollars": 201.65,
+      }
+    ]
+  }
+  dan: {
+    firstName: "Dan",
+    lastName: "East",
+    dateJoined: "2019-03-15",
+    profileImage: "https://media.licdn.com/dms/image/C4D03AQFfPiAo0r6iyg/profile-displayphoto-shrink_800_800/0?e=1560988800&v=beta&t=TYPR97EfA-28VyLh_a2CyDLqZZ7q8VY8kr4AB-ETepo"
+    email: "dan@dan.com",
+    residentialAddressStreetNumber: "7",
+    residenitalAddressStreet: "Woodward",
+    residentialAddressStreetType: "Ave",
+    residentialAddressSuburb: "Stanhope Gardens",
+    residentialAddressState: "NSW",
+    residentialAddressPostcode: "2768",
+    residentialAddressCountry: "Australia",
+    mobilePretty: "0402 239 471",
+    "entities_linked_to": [
+      {
+        "entityId": 2,
+        "role": "owner"
+        "admin": true,
+        "type": "trust",
+        "name": "Bebeje Pty Ltd",
+        "accounts": [
+          "accountId": 5,
+          "accountType": "trust",
+          "accountNickName": "Bebeje Pty Ltd",
+          "initialInvestmentAmount": 25000,
+          "bankAccountName": "Bebeje Pty Ltd",
+          "bankAccountBSB": "923 100",
+          "bankAccountNumber": "123456789",
+          "balanceDollars": 25307.95,
+        ]
+      },
+    ],
+  },
+}
