f559b9ea18b97cd19fb33fbcffb7f0fd47d5f7d6
phoenix377 <phoenix.number377@gmail.com>
2019-05-08 06:45:06 +0800

    hooking up the signup/login screens to cognito

--- FutureRenewables/src/configs/environments.js
+++ FutureRenewables/src/configs/environments.js
@@ -9,6 +9,12 @@ export const APP_ENV = {
     kleberKey: 'RK-A7331-D6238-189D0-FC8A7-C25ED-1B676-3133A-3122F',
     kleberGUID: 'a4ebd32f-2cef-4389-9ae2-288b3b7a9ee2',
     darkSkyKey: 'ff42a48b5252012ba41104cadbb0a417',
+    cognito: {
+      REGION: 'ap-southeast-2',
+      USER_POOL_ID: 'ap-southeast-2_cGLx2cEHC',
+      APP_CLIENT_ID: '19slupqo9243i9s6s79kk7hd8r',
+      IDENTITY_POOL_ID: 'ap-southeast-2:d77534a6-51d1-4dc8-bab5-df5f4a22b2e9',
+    },
   },
   production: {
     env: 'production',
@@ -17,5 +23,11 @@ export const APP_ENV = {
     kleberKey: 'RK-A7331-D6238-189D0-FC8A7-C25ED-1B676-3133A-3122F',
     kleberGUID: 'a4ebd32f-2cef-4389-9ae2-288b3b7a9ee2',
     darkSkyKey: 'ff42a48b5252012ba41104cadbb0a417',
+    cognito: {
+      REGION: 'ap-southeast-2',
+      USER_POOL_ID: 'ap-southeast-2_cGLx2cEHC',
+      APP_CLIENT_ID: '19slupqo9243i9s6s79kk7hd8r',
+      IDENTITY_POOL_ID: 'ap-southeast-2:d77534a6-51d1-4dc8-bab5-df5f4a22b2e9',
+    },
   },
 };
