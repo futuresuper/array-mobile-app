3032e09befc354c68170929de06c9a4e542178eb
phoenix377 <phoenix.number377@gmail.com>
2019-07-09 09:28:14 +0800

    code clean up & react-native-svg install

--- FutureRenewables/android/app/src/main/java/com/futurerenewables/MainApplication.java
+++ FutureRenewables/android/app/src/main/java/com/futurerenewables/MainApplication.java
@@ -3,6 +3,7 @@ package com.futurerenewables;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNAWSCognitoPackage(),
             new LinearGradientPackage(),
             new MapsPackage(),
