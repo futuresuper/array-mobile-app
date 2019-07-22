70630a6b6ecae820d03868f3000f71df55bbf22d
phoenix377 <phoenix.number377@gmail.com>
2019-07-22 08:02:08 +0800

    install react-native-intercom

--- FutureRenewables/android/app/src/main/java/com/futurerenewables/MainApplication.java
+++ FutureRenewables/android/app/src/main/java/com/futurerenewables/MainApplication.java
@@ -3,6 +3,7 @@ package com.futurerenewables;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.robinpowered.react.Intercom.IntercomPackage;
 import com.horcrux.svg.SvgPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new IntercomPackage(),
             new SvgPackage(),
             new RNAWSCognitoPackage(),
             new LinearGradientPackage(),
