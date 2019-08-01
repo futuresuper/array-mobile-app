bf203634a46e362a53014860ef16b8e6b8c53fda
Andrew Sellen <andrew@myfuturesuper.com.au>
2019-08-01 12:47:35 +0000

    Changed nav back to original

--- FutureRenewables/src/Screens/SignUpLogin/AppLanding.js
+++ FutureRenewables/src/Screens/SignUpLogin/AppLanding.js
@@ -100,7 +100,7 @@ class AppLanding extends Component {
             <Button
               block
               onPress={() => {
-                screenProps.navigateTo(routeNames.SIGN_UP_LOGIN);
+                screenProps.navigateTo(routeNames.BUILD_YOUR_SAVINGS);
               }}
             >
               <Text>Next</Text>
