9b1c54a535d838f5cbac8221ae43f7c0c4597225
phoenix377 <phoenix.number377@gmail.com>
2019-11-06 07:02:21 +0800

    underline PDS hyperlink

--- src/Screens/SignUpLogin/BuildYourSaving.js
+++ src/Screens/SignUpLogin/BuildYourSaving.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import {
   View,
   Image,
+  Linking,
 } from 'react-native';
 
 import {
@@ -25,8 +26,14 @@ import SafeAreaView from 'src/Components/SafeAreaView';
 import buildYourSaving from './images/buildYourSaving.png';
 import styles from './styles';
 
+
 // eslint-disable-next-line react/prefer-stateless-function
 class BuildYourSaving extends Component {
+  handlePDSPress() {
+    Linking.openURL('https://www.futurerenewablesfund.com.au/pds').catch((err) => console.error("Couldn't load page", err));
+  }
+
+
   render() {
     const { screenProps } = this.props;
 
@@ -52,9 +59,17 @@ class BuildYourSaving extends Component {
             <Text style={[sg.contentMarginH, sg.fS11, sg.colorDark3alpha, sg.textCenter]}>
             ^ Target Return is after fees and expenses and including distributions. The Fund Manager has a reasonable basis for setting the target return,
             however it is a ‘target’ only.  It is not intended as a projection of likely future returns and is not a guarantee.
-            The value of your investment can rise and fall. Please refer to the PDS for further information.
+            The value of your investment can rise and fall. Please refer to the
+              <Text
+                onPress={() => this.handlePDSPress()}
+                style={[
+                  { textDecorationLine: 'underline', textDecorationStyle: 'solid' },
+                  sg.fS11, sg.colorDark3alpha]}
+              >
+                {' PDS '}
+              </Text>
+              for further information.
             </Text>
-
             <Button
               style={[sg.contentMargin, sg.mT5]}
               block
