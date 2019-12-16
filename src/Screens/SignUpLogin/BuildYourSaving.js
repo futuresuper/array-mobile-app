63b48ce6a23fe3a72efa9d8cc2812e8950726575
Andrew S <andrewmsellen@gmail.com>
2019-12-16 11:30:03 +1100

    Text change on Build Savings 2

--- src/Screens/SignUpLogin/BuildYourSaving.js
+++ src/Screens/SignUpLogin/BuildYourSaving.js
@@ -62,14 +62,16 @@ class BuildYourSaving extends Component {
             ^ Target Return is after fees and expenses and including distributions. The Fund Manager has a reasonable basis for setting the target return,
             however it is a ‘target’ only.  It is not intended as a projection of likely future returns and is not a guarantee.
             The value of your investment can rise and fall. Please refer to the
+              {' '}
               <Text
                 onPress={() => this.handlePDSPress()}
                 style={[
                   { textDecorationLine: 'underline', textDecorationStyle: 'solid' },
                   sg.fS11, sg.colorDark3alpha]}
               >
-                {' PDS '}
+                {'PDS'}
               </Text>
+              {' '}
               for further information.
             </Text>
             <Button
